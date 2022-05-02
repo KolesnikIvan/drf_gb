2521d4d8c2133f83adb690c1b493a7a718abc842
Kolesnik Ivan <p3464ba@gmail.com>
2022-05-02 20:20:27 +0300

    without commented strings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -43,25 +17,6 @@ class App extends React.Component{
   // componentDidMount - это метод выполняемый при отображении
   // и содержащий логику этого процесса
   componentDidMount() {
-    // const users = [
-    //   {
-    //     'username': 'thrillerist',
-    //     'firstname': 'Fyodor',
-    //     'lastname': 'Dostoevskiy',
-    //     'email': 'fdostoev@itcompany.com'
-    //   },
-    //   {
-    //     'username': 'romantic',
-    //     'firstname': 'Alexander',
-    //     'lastname': 'Green',
-    //     'email': 'alex_green@itcompany.com'
-    //   },
-    // ]
-    // this.setState(
-    //   {
-    //     'users': users
-    //   }
-    // )
     axios.get('http://127.0.0.1:8000/api/users')
       .then(response => {
         const users =  response.data
