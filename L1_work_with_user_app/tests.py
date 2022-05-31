0d2433e9358d65242210627a1500e598c4403e80
Kolesnik Ivan <p3464ba@gmail.com>
2022-05-31 14:33:13 +0300

    lesson8

--- L1_work_with_user_app/tests.py
+++ L1_work_with_user_app/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+import json
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate
+from django.contrib.auth.models import User
+from .models import MyUser
+from .views import MyUserViewSet
+import random
+from faker import Faker
+from mimesis import Person
 
 # Create your tests here.
+
+class TestMyUser(TestCase):
+    
+    def test_get_user_list(self):
+        print('test get user list')
+        factory = APIRequestFactory()
+        request = factory.get('/api/users_les1/')
+        view = MyUserViewSet.as_view({'get':'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(self.assertEqual(response.status_code, status.HTTP_200_OK))
+
+    def test_post_user(self):
+        print('post test is on')
+        pers = Person
+        factory = APIRequestFactory()
+        # request = factory.post('/api/users/', 
+        #     {'username':Faker().name, 
+        #     'firstname':Faker().first_name, 
+        #     'lastname': Faker().last_name,
+        #     'email': Faker().email,
+        #     # 'position': Person().worldview(),
+        #     },
+        #     format='json')
+        request = factory.post('/api/users/', {'username':'Pushkin', 'fristname':'Alex', 'lastname':'Pushkin', 'email':'pushkin@gmail.com'}, format='json')
+        view = MyUserViewSet.as_view({'post':'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        print('authenticated post test is on')
+        admin = MyUser.objects.create_superuser(
+                        'superuser', 
+                        'super@itcom.com', 
+                        'superman',
+                        )
+        force_authenticate(request, admin)
+        view_auth = MyUserViewSet.as_view({'post':'create'})
+        response = view_auth(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- L3_serializers/tests.py
+++ L3_serializers/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+import json
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
+from django.contrib.auth.models import User
+from .views import ProjectViewSetLes1, TodoViewSetLes1
+from .models import Project, Todo
+from L1_work_with_user_app.models import MyUser
+import mixer
 
 # Create your tests here.
+
+class TestProjectViewSet(TestCase):
+
+    def test_apiclient(self):
+        print('test with API client')
+        users=[]
+        for i in range(3):
+            user = MyUser.objects.create(username='user'+str(i), email='user'+str(i))
+            user.save()
+            users.append(user)
+        project = Project.objects.create(name='TestProject', 
+                        link_to_repo='http://TestProject.com')
+        # https://stackoverflow.com/questions/50015204/direct-assignment-to-the-forward-side-of-a-many-to-many-set-is-prohibited-use-e
+        project.users.set(users)
+        client = APIClient()
+        response = client.get(f'/api/projects_les1/{project.uid}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print('APIClient test ended')
+
+
+class TestTOdoViewset(APITestCase):
+    def test_todo_using_apitestcase(self):
+        print('start testinig todo view with apitestcase')
+        response = self.client.get('/api/todos_les4/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print('apitestcase finished')
+
+    def test_todo_using_mixer(self):
+        print('test with mixer')
+        todo = mixer.blend(Todo)
+        admin = MyUser.objects.create_superuser('admin', 'adminadmin@gmail.com', 'geekbrains')
+        self.client.login(username='admin', password='geekbrains')
+        response = self.client.put(f'/api/todos_les4/{todo.uid}', 
+                                    {'text':'hello world', 
+                                    'project':todo.project.uid,
+                                    })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
